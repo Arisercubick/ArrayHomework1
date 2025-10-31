bfb5158203eaad4dafd793d88bcddb1d1bf88533
Aris John <108643071+Arisercubick@users.noreply.github.com>
2025-10-31 12:30:13 -0400

    Add Scanner instance for user input

--- Arrays.java
+++ Arrays.java
@@ -2,6 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 public class Arrays {
   private static Random rand = new Random();
+  private static Scanner reader = new Scanner();
   public static void main(String[] args) {
     System.out.print("How long should the array be: ");
     int arrLength = reader.nextInt();
