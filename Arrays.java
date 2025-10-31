dac927bf62994c41187d33286c8b0bf9c85d79ac
Aris John <108643071+Arisercubick@users.noreply.github.com>
2025-10-31 12:18:07 -0400

    Initialize array with random values and print
    Add array initialization and print functionality.

--- Arrays.java
+++ Arrays.java
@@ -6,5 +6,20 @@ public class Arrays {
     System.out.print("How long should the array be: ");
     int arrLength = reader.nextInt();
     int[] arr = new int[arrLength];
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = rand.nextInt();
+    }
   }
+
+  public static void printArray(int[] arr) {
+    System.out.print("\n");
+    for (int i = 0; i < arr.length; i++) {
+      if (i == 0) {
+        System.out.print("[" + arr[i] + ", ");
+      } else if (i == arr.length-1) {
+        System.out.print(arr[i] + "]");
+      } else {
+        System.out.print(arr[i] + ", ");
+      }
+    }
 }
