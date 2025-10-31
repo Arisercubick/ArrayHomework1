b53547de42432bf573641fae33bf42b9c09afba5
Aris John <108643071+Arisercubick@users.noreply.github.com>
2025-10-31 12:12:17 -0400

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,5 +1,10 @@
+import java.util.Random;
+import java.util.Scanner;
 public class Arrays {
+  private static Random rand = new Random();
   public static void main(String[] args) {
-
+    System.out.print("How long should the array be: ");
+    int arrLength = reader.nextInt();
+    int[] arr = new int[arrLength];
   }
 }
