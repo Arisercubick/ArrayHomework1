75a2092bb6b156bda99509eec283cbabf15cd16a
Aris John <108643071+Arisercubick@users.noreply.github.com>
2025-10-31 12:31:37 -0400

    Fix Random and Scanner initialization in Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,8 +1,8 @@
 import java.util.Random;
 import java.util.Scanner;
 public class Arrays {
-  private static Random rand = new Random(System.in);
-  private static Scanner reader = new Scanner();
+  private static Random rand = new Random();
+  private static Scanner reader = new Scanner(System.in);
   public static void main(String[] args) {
     System.out.print("How long should the array be: ");
     int arrLength = reader.nextInt();
