21b8a746b94627a44f98c62a2dfcc612e846e20e
Aris John <108643071+Arisercubick@users.noreply.github.com>
2025-10-31 12:31:07 -0400

    Initialize Random with System.in as seed

--- Arrays.java
+++ Arrays.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 public class Arrays {
-  private static Random rand = new Random();
+  private static Random rand = new Random(System.in);
   private static Scanner reader = new Scanner();
   public static void main(String[] args) {
     System.out.print("How long should the array be: ");
