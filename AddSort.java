97a64c3c62df69086b091584f7de53308e731193
Aris John <108643071+Arisercubick@users.noreply.github.com>
2025-11-03 08:39:58 -0500

    Add Random import and modify user input method

--- AddSort.java
+++ AddSort.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
+import java.util.Random;
 public class AddSort {
   private static Scanner reader = new Scanner(System.in);
+  private static Random rand = new Random();
   public static void main(String[] args) {
-
+    System.out.print("How big is the array: ");
+    int input = (int) reader.nextDouble();
   }
 }
