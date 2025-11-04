8d157cc52cc651cfdc505433dc0baf812dd62f21
Aris John <108643071+Arisercubick@users.noreply.github.com>
2025-11-04 10:48:11 -0500

    Modify inputUser to fill array based on user input
    Refactor inputUser method to initialize array after user input.

--- AddSort.java
+++ AddSort.java
@@ -4,18 +4,26 @@ public class AddSort {
   private static Scanner reader = new Scanner(System.in);
   private static Random rand = new Random();
   public static void main(String[] args) {
+    inputUser();
+  }
+  public static void inputUser() {
     System.out.print("How big is the array: ");
     int input = (int) reader.nextDouble();
-    int[] arr = new int[input];
-
     System.out.print("\nMinimum number:");
     int min = -((int) reader.nextDouble());
     System.out.print("\nMaximum number:");
     int max = (int) reader.nextDouble();
-    
+    int[] arr = fillArr(max, min, input);
+  }
+
+  public static int[] fillArr(int max, int min, int count) {
+    int[] arr = new int[count];
     for (int i = 0; i < arr.length; i++) {
       arr[i] = rand.nextInt(max+1+min)-min;
     }
+  }
+
+  public static void printArr(int[] arr) {
     System.out.print("\nYour Array:\n");
     for (int i = 0; i < arr.length; i++) {
       if (i == 0) {
