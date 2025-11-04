f1ea4b403d885fd3cadf76ab2732a717e8462598
Aris John <108643071+Arisercubick@users.noreply.github.com>
2025-11-04 10:25:44 -0500

    Fix random number generation in AddSort.java

--- AddSort.java
+++ AddSort.java
@@ -14,7 +14,7 @@ public class AddSort {
     int max = (int) reader.nextDouble();
     
     for (int i = 0; i < arr.length; i++) {
-      arr[i] = rand.nextInt(max+1)-min;
+      arr[i] = rand.nextInt(max+1+min)-min;
     }
     System.out.print("\nYour Array:\n");
     for (int i = 0; i < arr.length; i++) {
